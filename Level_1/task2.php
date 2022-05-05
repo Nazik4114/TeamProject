6c68cd2eb6d9e1f5964762c9026b5680246bb6b3
Nazik4114 <nazikban14@gmail.com>
2022-05-05 16:47:07 +0300

    Added Level_1 Task_2

---
+++ Level_1/task2.php
@@ -0,0 +1,24 @@
+<?php
+$min = 10;
+$max = 99;
+$count1 = 3;
+$count2 = 5;
+
+for ($i=0; $i<$count1; $i++) {
+  $a[] = rand($min, $max);
+  echo $a[$i]." ";
+}
+echo PHP_EOL;
+for ($i=0; $i<$count2; $i++) {
+  $b[] = rand($min, $max);
+  echo $b[$i] . " ";
+}
+echo PHP_EOL;
+$c = array_merge($a,$b);
+$c = array_unique($c);
+sort($c);
+echo "Результат: ".implode(" ", $c).PHP_EOL;
+echo "К-сть ел-тів: ".count($c)." Сума ел-тів: ".array_sum($c);
+
+
+
