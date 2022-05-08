4f52ec0f631390d1da35254516ccde91d1832be9
Andrii Kondratenko <andrii.kondratenko@smile-ukraine.com>
2022-05-08 17:06:57 +0300

    Fix error print array Task 1

--- Level_1/Task_1.php
+++ Level_1/Task_1.php
@@ -5,19 +5,22 @@ define('FINISH_NUMBER', 30);
 
 $numbers = [];
 echo "\$numbers = [ ";
-for ($i=0; $i < COUNT_NUMBERS; $i++) {
-  $numbers[] = random_int(START_NUMBER,FINISH_NUMBER);
-  echo $numbers[$i].", ";
+for ($i = 0; $i < COUNT_NUMBERS; $i++) {
+  $numbers[] = random_int(START_NUMBER, FINISH_NUMBER);
+  echo $numbers[$i] . ", ";
 }
-echo "]".PHP_EOL;
+echo "]" . PHP_EOL;
 
-echo "Результат:".PHP_EOL;
 $pos_array_positive = 0;
-for ($i=0; $i < COUNT($numbers); $i++) {
+for ($i = 0; $i < COUNT($numbers); $i++) {
   if ($numbers[$i] >= 0) {
     $tmp = $numbers[$i];
     $numbers[$i] = $numbers[$pos_array_positive];
     $numbers[$pos_array_positive++] = $tmp;
   }
-  echo $numbers[$i]." ";
+}
+
+echo "Результат:" . PHP_EOL;
+for ($i = 0; $i < COUNT($numbers); $i++) {
+  echo $numbers[$i] . " ";
 }
